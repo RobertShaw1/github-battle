b6a43dccece2278d23afb753f6137abcf08928a5
Robert Shaw <r.shaw1@yahoo.com>
2017-07-04 15:06:15 -0500

    built out battle component and added styling

--- app/components/App.js
+++ app/components/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
                     <Nav />
                     <Switch>
                         <Route exact path='/' component={Home} />
-                        <Route path='/battle' component={Battle} />
+                        <Route exact path='/battle' component={Battle} />
                         <Route path='/popular' component={Popular} />
                         <Route render={function() {
                             return <p>Not Found</p>
--- app/components/Home.js
+++ app/components/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
     render() {
         return (
             <div className='home-container'>
-                <h1>Github Battle: Battle your frineds...and stuff.</h1>
+                <h1>Github Battle: Battle your friends...and stuff.</h1>
 
                 <Link className='button' to='/battle'>
                     Battle
