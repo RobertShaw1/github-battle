406744eed93bd4460c22cecbcc5d6ec901ce2717
Robert Shaw <r.shaw1@yahoo.com>
2017-10-21 17:50:41 -0500

    refactored both Battle Component and PlayerInput Component using ES6 syntax

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,81 +1,79 @@
-const React = require('react');
-const PropTypes = require('prop-types');
-const PlayerInput = require('./PlayerInput')
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import PlayerInput from './PlayerInput';
 
 
+export default class Battle extends Component {
+  constructor(props) {
+    super(props);
 
-
-class Battle extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            playerOneName: '',
-            playerTwoName: '',
-            playerOneImage: null,
-            playerTwoImage: null
-        }
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
     }
-    handleSubmit(id, username) {
-        this.setState(function() {
-            let newState = {};
-            newState[id + 'Name'] = username;
-            newState[id + 'Image'] = `https://github.com/${username}.png?size=200`;
-            return newState;
-        });
-    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
 
-    handleReset(id) {
-        this.setState(function() {
-            let newState = {};
-            newState[id + 'Name'] = '';
-            newState[id + 'Image'] = null;
-            return newState;
-        })
-    }
-    render() {
-        let playerOneName = this.state.playerOneName;
-        let playerTwoName = this.state.playerTwoName;
-        let playerOneImage = this.state.playerOneImage;
-        let playerTwoImage = this.state.playerTwoImage;
+  handleSubmit(id, username) {
+    this.setState(function () {
+      let newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = `https://github.com/${username}.png?size=200`;
+      return newState;
+    });
+  }
 
-        return (
-            <div>
-                <div className='row'>
-                    {!playerOneName &&
-                        <PlayerInput
-                            id='playerOne'
-                            label='Player One'
-                            onSubmit={this.handleSubmit}
-                        />}
-                    {!playerTwoName &&
-                        <PlayerInput
-                            id='playerTwo'
-                            label='Player Two'
-                            onSubmit={this.handleSubmit}
-                        />}
-                    
-                    {playerOneImage !== null &&
-                        <PlayerPreview
-                            avatar={playerOneImage}
-                            username={playerOneName}
-                            onReset={this.handleReset}
-                            id='playerOne'
-                        />}
-                    
-                    {playerTwoImage !== null &&
-                        <PlayerPreview
-                            avatar={playerTwoImage}
-                            username={playerTwoName}
-                            onReset={this.handleReset}
-                            id='playerTwo'
-                        />}
-                </div>
-            </div>
-        )
-    }
+  handleReset(id) {
+    this.setState(function () {
+      let newState = {};
+      newState[id + 'Name'] = '';
+      newState[id + 'Image'] = null;
+      return newState;
+    })
+  }
+
+  render() {
+    let playerOneName = this.state.playerOneName;
+    let playerTwoName = this.state.playerTwoName;
+    let playerOneImage = this.state.playerOneImage;
+    let playerTwoImage = this.state.playerTwoImage;
+
+    return (
+      <div>
+        <div className='row'>
+          {!playerOneName &&
+            <PlayerInput
+              id='playerOne'
+              label='Player One'
+              onSubmit={this.handleSubmit}
+            />}
+          {!playerTwoName &&
+            <PlayerInput
+              id='playerTwo'
+              label='Player Two'
+              onSubmit={this.handleSubmit}
+            />}
+
+          {playerOneImage !== null &&
+            <PlayerPreview
+              avatar={playerOneImage}
+              username={playerOneName}
+              onReset={this.handleReset}
+              id='playerOne'
+            />}
+
+          {playerTwoImage !== null &&
+            <PlayerPreview
+              avatar={playerTwoImage}
+              username={playerTwoName}
+              onReset={this.handleReset}
+              id='playerTwo'
+            />}
+        </div>
+      </div>
+    )
+  }
 }
-
-module.exports = Battle;
