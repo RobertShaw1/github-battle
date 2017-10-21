a67317812f23f66272336c13ddf920f29bc095c5
Robert Shaw <r.shaw1@yahoo.com>
2017-10-21 17:19:55 -0500

    refactored index.js and App component using ES6 syntax

--- app/components/App.js
+++ app/components/App.js
@@ -1,33 +1,29 @@
-const React = require('react');
-const ReactRouter = require('react-router-dom');
-const Router = ReactRouter.BrowserRouter;
-const Route = ReactRouter.Route;
-const Switch = ReactRouter.Switch;
-const Nav = require('./Nav');
-const Home = require('./Home');
-const Battle = require('./Battle');
-const Popular = require('./Popular');
+/** NODE MODULES */
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+/** LOCAL MODULES */
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
+import Popular from './Popular';
 
 
 //Create App component
-class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <div className='container'>
-                    <Nav /> 
-                    <Switch>
-                        <Route exact path='/' component={Home} />
-                        <Route exact path='/battle' component={Battle} />
-                        <Route path='/popular' component={Popular} />
-                        <Route render={function() {
-                            return <p>Not Found</p>
-                        }}/>
-                    </Switch>
-                </div>
-            </Router>
-        )
-    }
+export default function App() {
+  return (
+    <Router>
+      <div className='container'>
+          <Nav /> 
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/battle' component={Battle} />
+              <Route path='/popular' component={Popular} />
+              <Route render={function() {
+                  return <p>Not Found</p>
+              }}/>
+          </Switch>
+      </div>
+    </Router>
+  )
 }
-
-module.exports = App;
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-require('./index.css');
-const App = require('./components/App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
 
 //Render App component to the DOM
 ReactDOM.render(
