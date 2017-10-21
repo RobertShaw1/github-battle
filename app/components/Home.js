576c04e1def73e833f5e1b1ba0983d218291a4f9
Robert Shaw <r.shaw1@yahoo.com>
2017-10-21 17:24:19 -0500

    refactored Home component using ES6 syntax

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,18 +1,14 @@
-const React = require('react');
-const Link = require('react-router-dom').Link;
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-class Home extends React.Component {
-    render() {
-        return (
-            <div className='home-container'>
-                <h1>Github Battle: Battle your friends...and stuff.</h1>
+export default function Home() {
+  return (
+    <div className='home-container'>
+      <h1>Github Battle: Battle your friends...and stuff.</h1>
 
-                <Link className='button' to='/battle'>
-                    Battle
-                </Link>
-            </div>
-        )
-    }
+      <Link className='button' to='/battle'>
+          Battle
+      </Link>
+    </div>
+  )
 }
-
-module.exports = Home;
