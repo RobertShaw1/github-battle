e60360d43b1c074f0f8a198398ec05da9bc2d970
Robert Shaw <r.shaw1@yahoo.com>
2017-10-22 18:40:21 -0500

    addded route for Results component

--- app/components/App.js
+++ app/components/App.js
@@ -7,6 +7,7 @@ import Nav from './Nav';
 import Home from './Home';
 import Battle from './Battle';
 import Popular from './Popular';
+import Results from './Results';
 
 
 //Create App component
@@ -18,6 +19,7 @@ export default function App() {
           <Switch>
               <Route exact path='/' component={Home} />
               <Route exact path='/battle' component={Battle} />
+              <Route path='/battle/results' component={Results} />
               <Route path='/popular' component={Popular} />
               <Route render={function() {
                   return <p>Not Found</p>
