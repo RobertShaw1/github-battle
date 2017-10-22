b60cfaeec80921c8285a50f1386a7aaa2854af04
Robert Shaw <r.shaw1@yahoo.com>
2017-10-22 18:39:58 -0500

    created scaffold for Results component

---
+++ app/components/Results.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class Results extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Results
+      </div>
+    )
+  }
+}
