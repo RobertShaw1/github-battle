46060ab6bce73b3b55e4579698bc2d6661bbc908
Robert Shaw <r.shaw1@yahoo.com>
2017-10-22 20:34:29 -0500

    added battle function to api call

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,12 +1,63 @@
-const axios = require('axios');
+import axios from 'axios';
 
-module.exports = {
-    fetchPopularRepos: function(language) {
-        let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
+function getProfile(username) {
+  return axios.get(`https://api.github.com/users/${username}`)
+    .then(user => user.data);
+}
 
-        return axios.get(encodedURI)
-            .then(function(response) {
-                return response.data.items;
-            });
+function getRepos(username) {
+  return axios.get(`https://api.github.com/users/${username}/repos?per_page=100`);
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce((count, repo) => {
+    return count + repo.stargazers_count;
+  },0)
+}
+
+function calculateScore(profile, repos) {
+  const {followers} = profile;
+  const totalStars = getStarCount(repos);
+
+  return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(data => {
+    const profile = data[0];
+    const repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
     }
+  })
+}
+
+function sortPlayers(players) {
+  return players.sort((a,b) => b.score - a.score);
+}
+
+export default {
+  battle: function(players) {
+    return axios.all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError)
+  },
+  fetchPopularRepos: function (language) {
+    let encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
+
+    return axios.get(encodedURI)
+      .then(function (response) {
+        return response.data.items;
+      });
+  },
 }
